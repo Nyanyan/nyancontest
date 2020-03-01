df69e43ae07088d6736a712b3587c74d76a5c6e1
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-01 11:18:49 +0900

    modified

--- assets/js/getscramble.js
+++ assets/js/getscramble.js
@@ -1,17 +1,26 @@
-mple.js
-JavaScript
-$(window).load(function () {
-    $.get('scramble.csv',function(data){
-        var csv = $.csv()(data);
-        var itemlist = '';
+//CSVファイルを読み込む関数getCSV()の定義
+function getCSV(){
+    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", "../../scramble.csv", true); // アクセスするファイルを指定
+    req.send(null); // HTTPリクエストの発行
+	
+    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
+    req.onload = function(){
+	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    }
+}
+ 
+// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+    var result = []; // 最終的な二次元配列を入れるための配列
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+ 
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+ 
+    alert(result[1][2]); // 300yen
+}
 
-        $(csv).each(function(index){
-                itemlist += '<li class="list-item">';
-                itemlist += '<p class="date">' + this[0] + '</p>';
-                itemlist += '<p class="title"><a href="' + this[1] + '">' + this[2] + '</a></p>';
-                itemlist += '<p class="body">' + this[3] + '</p>';
-                itemlist += '</li>';
-	})
-	$("#itemlist").append(itemlist);
-    });
-});
+getCSV(); //最初に実行される
