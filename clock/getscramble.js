484294dfad532c5d9631c6cfd8883cf0196ae538
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-03 15:10:51 +0900

    modified

--- clock/getscramble.js
+++ clock/getscramble.js
@@ -1,30 +1,40 @@
-//CSVファイルを読み込む関数getCSV()の定義
-function getCSV(){
-    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
-    req.open("get", "../scramble.csv", true); // アクセスするファイルを指定
-    req.send(null); // HTTPリクエストの発行
-    
-    alert(req.responseText);
-    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
-    req.onload = function(){
-	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
-    }
-}
- 
-// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
-function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
-    //var arg = process.argv;
-    var result = []; // 最終的な二次元配列を入れるための配列
-    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
- 
-    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
-    for(var i=0;i<tmp.length;++i){
-        result[i] = tmp[i].split(',');
-    }
- 
-    //alert(arg);
-    alert(result); // 300yen
-    document.getElementById('result').innerHTML = result.join('');
+const outputElement = document.getElementById('scramble');
+
+function getCsvData(dataPath) {
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', (event) => {
+        const response = event.target.responseText;
+        convertArray(response);
+    });
+    request.open('GET', dataPath, true);
+    request.send();
 }
 
-getCSV(); //最初に実行される
+function convertArray(data) {
+    const dataArray = [];
+    const dataString = data.split('\n');
+    for (let i = 0; i < dataString.length; i++) {
+        dataArray[i] = dataString[i].split(',');
+    }
+    let insertElement = '';
+    let flag = false;
+    dataArray.forEach((element) => {
+        //insertElement += '<tr>';
+        element.forEach((childElement) => {
+            if (flag) {
+                insertElement += `<tr><td>${childElement}</td></tr>`
+            }
+            if (childElement == 'Clock'){
+                flag = true;
+            }
+        });
+        if (flag){
+            break;
+        }
+        //insertElement += '</tr>';
+    });
+    //alert(insertElement)
+    outputElement.innerHTML = insertElement;
+}
+
+getCsvData('../scramble.csv');
