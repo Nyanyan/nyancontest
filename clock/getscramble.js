0c402df54520668a386e9f6e4ca18e198fd4e9f4
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-03 15:32:46 +0900

    modified

--- clock/getscramble.js
+++ clock/getscramble.js
@@ -1,19 +1,35 @@
-function readCsv(data) {
-    var target = '#scramble';
-    var csv = $.csv.toArrays(data);
-    var insert = '';
-    $(csv).each(function() {
-        if (this.length > 0) {
-            insert += '<tr>';
-            $(this).each(function() {
-                insert += '<td>' + this + '</td>';
-            });
-            insert += '</tr>';
-        }
+const outputElement2 = document.getElementById('scramble');
+
+function getCsvData(dataPath) {
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', (event) => {
+        const response = event.target.responseText;
+        convertArray(response);
     });
-    $(target).append(insert);
+    request.open('GET', dataPath, true);
+    request.send();
 }
-var csvfile = 'test.csv';
-$(function(){
-    $.get(csvfile, readCsv, 'text');
-});
+
+function convertArray(data) {
+    const dataArray = [];
+    const dataString = data.split('\n');
+    for (let i = 0; i < dataString.length; i++) {
+        dataArray[i] = dataString[i].split(',');
+    }
+    let insertElement = '';
+    let flag = false;
+    dataArray.forEach((element) => {
+        //insertElement += '<tr>';
+        element.forEach((childElement) => {
+            alert(childElement)
+        });
+        if (flag){
+            break
+        }
+        //insertElement += '</tr>';
+    });
+    //alert(insertElement)
+    outputElement2.innerHTML = insertElement;
+}
+
+getCsvData('../scramble.csv');
