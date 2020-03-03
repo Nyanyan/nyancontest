076b09e93859900f131a92c9f98c1d05438162f6
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-03 14:24:53 +0900

    modified

--- clock/getdata.js
+++ clock/getdata.js
@@ -1,30 +1,16 @@
-const outputElement = document.getElementById('output_csv');
-
-function getCsvData(dataPath) {
- const request = new XMLHttpRequest();
- request.addEventListener('load', (event) => {
-  const response = event.target.responseText;
-  convertArray(response);
- });
- request.open('GET', dataPath, true);
- request.send();
-}
-
-function convertArray(data) {
- const dataArray = [];
- const dataString = data.split('\n');
- for (let i = 0; i < dataString.length; i++) {
-  dataArray[i] = dataString[i].split(',');
- }
- let insertElement = '';
- dataArray.forEach((element) => {
-  insertElement += '<tr>';
-  element.forEach((childElement) => {
-   insertElement += `<td>${childElement}</td>`
-  });
-  insertElement += '</tr>';
- });
- outputElement.innerHTML = insertElement;
-}
-
-getCsvData('data.csv');
+$.when(
+    $.ajax({
+        url: 'data.csv',
+        cache: false,
+        dataType: "text",
+        beforeSend : function(xhr) {
+            xhr.overrideMimeType('text/plain;charset=Shift_JIS');
+        },
+    })
+)
+.done(function(data) {
+    data=$.csv()(data[0]);
+})
+.fail(function() {
+    alert("データベースの読み込みエラーが発生しました。");
+});
