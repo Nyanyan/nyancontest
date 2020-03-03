ae8b939732c1694d9c628638035e225d825f9f08
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-03 16:08:17 +0900

    modified

--- clock/getscramble.js
+++ clock/getscramble.js
@@ -16,13 +16,11 @@ function convertArray(data) {
     for (let i = 0; i < dataString.length; i++) {
         dataArray[i] = dataString[i].split(',');
     }
-    let insertElement = '<tr><td>日時</td><td>名前</td><td>1st</td><td>3nd</td><td>3rd</td><td>4th</td><td>5th</td><td>avg</td></tr>';
+    let insertElement = '';
     dataArray.forEach((element) => {
-        insertElement += '<tr>';
         element.forEach((childElement) => {
-            insertElement += `<td>${childElement}</td>`
+            insertElement += `<tr><td>${childElement}</td></tr>`
         });
-        insertElement += '</tr>';
     });
     //alert(insertElement)
     outputElement2.innerHTML = insertElement;
