cca8e1f19d3d8afa4461ed89904858fb79814083
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-03 13:32:11 +0900

    modified

--- clock/getdata.js
+++ clock/getdata.js
@@ -1,31 +1,13 @@
-const outputElement = document.getElementById('getdata');
+const outputElement = document.getElementById('output_csv');
 
 function getCsvData(dataPath) {
  const request = new XMLHttpRequest();
  request.addEventListener('load', (event) => {
   const response = event.target.responseText;
-  convertArray(response);
+  outputElement.innerHTML = response;
  });
  request.open('GET', dataPath, true);
  request.send();
 }
 
-function convertArray(data) {
- const dataArray = [];
- const dataString = data.split('\n');
- for (let i = 0; i < dataString.length; i++) {
-  dataArray[i] = dataString[i].split(',');
- }
- let insertElement = '';
- dataArray.forEach((element) => {
-  insertElement += '<tr>';
-  element.forEach((childElement) => {
-   insertElement += `<td>${childElement}</td>`
-  });
-  insertElement += '</tr>';
- });
- outputElement.innerHTML = insertElement;
-}
-
-alert('getting data')
 getCsvData('data.csv');
