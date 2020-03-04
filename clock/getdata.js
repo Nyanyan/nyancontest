85406d86dd887014ecf62259f77b7902a7829c96
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-04 16:02:08 +0900

    modified js: show your rank number

--- clock/getdata.js
+++ clock/getdata.js
@@ -16,13 +16,17 @@ function convertArray1(data) {
     for (let i = 0; i < dataString.length; i++) {
         dataArray[i] = dataString[i].split(',');
     }
-    let insertElement = '<tr><td>日時</td><td>名前</td><td>1st</td><td>2nd</td><td>3rd</td><td>4th</td><td>5th</td><td>avg</td></tr>';
+    let insertElement = '<tr><td>順位</td><td>日時</td><td>名前</td><td>1st</td><td>2nd</td><td>3rd</td><td>4th</td><td>5th</td><td>avg</td></tr>';
+    let i = 0;
     dataArray.forEach((element) => {
-        insertElement += '<tr>';
+        insertElement += '<tr><td>';
+        insertElement += String(i);
+        insertElement += '</td>'
         element.forEach((childElement) => {
             insertElement += `<td>${childElement}</td>`
         });
         insertElement += '</tr>';
+        i += 1;
     });
     outputElement1.innerHTML = insertElement;
 }
