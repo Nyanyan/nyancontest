d60d2ba15f86e09410c7cdd9ec4d6667e549e047
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 19:54:21 +0900

    updated

--- scramblegenerator.py
+++ scramblegenerator.py
@@ -15,20 +15,27 @@ def generatescramble(session):
     print(scramble)
     return scramble
 
+with open('scramble.csv', mode='a') as f:
+    f.write('')
+
 sessions = ['3x3', '2x2', '4x4', '5x5', '6x6', '7x7', '3BLD', '3OH', 'Clock', 'Megaminx', 'Pyraminx', 'Skewb', 'Square-1', '4BLD', '5BLD']
 timeout = 10
+events = [0, 1, 8, 13, 14]
 
-session = 8 #clock
-row = [sessions[session]]
-while len(row) < 6:
-    start = time.time()
-    for i in range(1, 6):
-        row.append(generatescramble(session))
-        if time.time() - start > timeout:
-            row = [sessions[session]]
-            print('retry')
-            break
-with open('scramble.csv', mode='w') as f:
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerow(row)
+for session in events:
+    #session = 8 #clock
+    row = [sessions[session]]
+    while len(row) < 6:
+        start = time.time()
+        for i in range(1, 6):
+            row.append(generatescramble(session))
+            if time.time() - start > timeout:
+                row = [sessions[session]]
+                print('retry')
+                time.sleep(10)
+                break
+    with open('scramble.csv', mode='a') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(row)
+    time.sleep(10)
 print('finished generating scramble')
