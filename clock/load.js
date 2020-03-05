deac13ca4ad6a453aa3c9f1ebb24f1aef46b6cc6
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 18:57:22 +0900

    cleaning

--- clock/load.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $.ajaxSetup({cache:false});
-    $('#body').load('competition.html');
-    $('#body').replace('DATE', 'REPLACED');
-});
---
+++ clock/makehtml.py
@@ -0,0 +1,20 @@
+import csv
+html = ''
+with open("../competitionformat.html", 'r',encoding="utf-8") as f:
+    html = f.read()
+#html = html.replace('SCRIPT_REPLACE_HERE', '            <script src="getdata.js"></script>\n            <script src="getdata_yesterday.js"></script>\n          <script src="getdate.js"></script>\n            <script src="getscramble.js"></script>')
+scramble = ''
+scramblearr = []
+with open("../scramble.csv", 'r', encoding="utf-8") as f:
+    scramblearr = list(csv.reader(f))
+flag = False
+for i in range(len(scramblearr)):
+    if scramblearr[i][0] == 'Clock':
+        flag = True
+    if flag:
+        for j in range(len(scramblearr[i])):
+            scramble += '<tr><td>' + str(scramblearr[i][j]) + '</td></tr>'
+        break
+html = html.replace('SCRAMBLE_REPLACE_HERE', scramble)
+with open("index.html", 'w') as f:
+    f.write(html)
