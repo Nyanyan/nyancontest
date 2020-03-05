abb031730336f235dac7d867f503537fcdd462f3
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 23:26:31 +0900

    deleting program

--- deletedata.py
+++ deletedata.py
@@ -1,11 +1,8 @@
 import csv
 
-sessions = ['3x3', '2x2', '4x4', '5x5', '6x6', '7x7', '3BLD', '3OH', 'Clock', 'Megaminx', 'Pyraminx', 'Skewb', 'Square-1', '4BLD', '5BLD']
-#events = [0, 1, 8, 13, 14]
-events = [8]
+events = ['Clock', 'Mirror3x3']
 
-for i in events:
-    event = sessions[i]
+for event in events:
     data = []
     with open(event + '/data.csv', mode='r') as f:
         tmp = f.readline()
