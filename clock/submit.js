8ca9f1b98643132018380e56d5a1feaeda0d108e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 13:55:40 +0900

    embedded form

--- clock/submit.js
+++ clock/submit.js
@@ -1,5 +1,4 @@
 function submitForm(){
-    alert(document.getElementById('name').value)
     if (document.getElementById('name').value == "" || document.getElementById('1st').value == "" || document.getElementById('2nd').value == "" || document.getElementById('3rd').value == "" || document.getElementById('4th').value == "" || document.getElementById('5th').value == "") {
         alert("入力漏れがあります");
         return false;
