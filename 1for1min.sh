a0cacdf9cdb865ab152c885dc03a76df8899a18c
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 23:45:47 +0900

    updated by hand

--- 1for1min.sh
+++ 1for1min.sh
@@ -4,7 +4,7 @@ cd /home/pi/nyancontest
 python3 getresult.py
 git add Clock/data.csv
 git add Mirror3x3/data.csv
-git commit -m 'upodated data'
+git commit -m 'updated data'
 python3 makeallhtml.py
 git add Clock/index.html
 git add Mirror3x3/index.html
