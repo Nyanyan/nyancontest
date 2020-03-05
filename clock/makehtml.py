1b41123997ae37a3b37b0f97cdb8221de63493ad
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 19:00:47 +0900

    cleaning

--- clock/makehtml.py
+++ clock/makehtml.py
@@ -1,11 +1,11 @@
 import csv
 html = ''
-with open("../competitionformat.html", 'r',encoding="utf-8") as f:
+with open("../competitionformat.html", 'r',encoding="cp932") as f:
     html = f.read()
 #html = html.replace('SCRIPT_REPLACE_HERE', '            <script src="getdata.js"></script>\n            <script src="getdata_yesterday.js"></script>\n          <script src="getdate.js"></script>\n            <script src="getscramble.js"></script>')
 scramble = ''
 scramblearr = []
-with open("../scramble.csv", 'r', encoding="utf-8") as f:
+with open("../scramble.csv", 'r', encoding="cp932") as f:
     scramblearr = list(csv.reader(f))
 flag = False
 for i in range(len(scramblearr)):
@@ -16,5 +16,5 @@ for i in range(len(scramblearr)):
             scramble += '<tr><td>' + str(scramblearr[i][j]) + '</td></tr>'
         break
 html = html.replace('SCRAMBLE_REPLACE_HERE', scramble)
-with open("index.html", 'w') as f:
+with open("index.html", 'w', encoding="cp932") as f:
     f.write(html)
