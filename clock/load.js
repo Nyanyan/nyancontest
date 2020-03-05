7de58829df7adb6c4987e91945c44702c040b48c
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 18:15:32 +0900

    cleaning

--- clock/load.js
+++ clock/load.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $.ajaxSetup({cache:false});
     $('#body').load('competition.html');
-    $('#body').CreateElement('<script src="getdata.js"></script>');
-    $('#body').CreateElement('<script src="getscramble.js"></script>');
-    $('#body').CreateElement('<script src="getdate.js"></script>');
-    $('#body').CreateElement('<script src="getdata_yesterday.js"></script>');
+    $('#body').append('<script src="getdata.js"></script>');
+    $('#body').append('<script src="getscramble.js"></script>');
+    $('#body').append('<script src="getdate.js"></script>');
+    $('#body').append('<script src="getdata_yesterday.js"></script>');
 });
