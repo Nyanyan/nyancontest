a3e826a11ed045f6831e8ae72a1d8d445b81e2f7
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 23:37:04 +0900

    upodated data

--- 1for1min.sh
+++ 1for1min.sh
@@ -2,6 +2,11 @@
 
 cd /home/pi/nyancontest
 python3 getresult.py
-git add clock/data.csv
+git add Clock/data.csv
+git add Mirror3x3/data.csv
 git commit -m 'upodated data'
+python3 makeallhtml.py
+git add Clock/index.html
+git add Mirror3x3/index.html
+git commit -m 'modified html'
 git push origin master
--- 1forday.sh
+++ 1forday.sh
@@ -5,8 +5,10 @@ python3 scramblegenerator.py
 git add scramble.csv
 git commit -m 'update scramble'
 python3 deletedata.py
-git add clock/data.csv
-git add clock/data_yesterday.csv
+git add Clock/data.csv
+git add Clock/data_yesterday.csv
+git add Mirror3x3/data.csv
+git add Mirror3x3/data_yesterday.csv
 git commit -m 'updated data'
 python3 dateupdate.py
 git add date.txt
