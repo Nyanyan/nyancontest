edb91d7b27c716f06d3ea77753320bb10b1e2522
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 13:48:50 +0900

    embedded form

--- clock/submit.js
+++ clock/submit.js
@@ -1,6 +1,6 @@
 function submitForm(){
-    alert(!document.getElementById('name'))
-    if (!document.getElementById('name') || !document.getElementById('1st') || !document.getElementById('2nd') || !document.getElementById('3rd') || !document.getElementById('4th') || !document.getElementById('5th')) {
+    alert(document.getElementById('name'))
+    if (!document.getElementById('name') == "" || document.getElementById('1st') == "" || document.getElementById('2nd') == "" || document.getElementById('3rd') == "" || document.getElementById('4th') == "" || document.getElementById('5th') == "") {
         alert("入力漏れがあります");
         return false;
     } else {
