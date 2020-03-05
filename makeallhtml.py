cf3eecb80bb9a4deb8f597a84d7ce2634355981a
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-05 20:57:32 +0900

    modified

--- makeallhtml.py
+++ makeallhtml.py
@@ -2,7 +2,7 @@ import subprocess
 
 sessions = ['3x3', '2x2', '4x4', '5x5', '6x6', '7x7', '3BLD', '3OH', 'Clock', 'Megaminx', 'Pyraminx', 'Skewb', 'Square-1', '4BLD', '5BLD']
 #events = [0, 1, 8, 13, 14]
-events = [8]
+events = [0, 8]
 
 for i in events:
     arg = sessions[i]
