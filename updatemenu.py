720717c631ba7d4e2e2b50ac38740bb0cb5e666e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-06 14:46:54 +0900

    menu

--- updatemenu.py
+++ updatemenu.py
@@ -1,8 +1,17 @@
 import codecs
 
 htmlpath = [['home.html', 'index.html']]
+replaceelements = [['menuformat.html', 'MENU_REPLACE_HERE']]
 
-for path in htmlpath:
+for path, replacepath in zip(htmlpath, replaceelements):
+    print(path, replacepath)
     html = ''
-    with codecs.open("competitionformat.html", 'r', 'utf-8', 'ignore') as f:
+    with codecs.open(path[0], 'r', 'utf-8', 'ignore') as f:
         html = f.read()
+    replaceelement = ''
+    with codecs.open(replacepath[0], 'r', 'utf-8', 'ignore') as f:
+        replaceelement = f.read()
+    html.replace(replacepath[1], replaceelement)
+
+    with codecs.open(path[1], 'w', 'utf-8', 'ignore') as f:
+        f.write(html)
