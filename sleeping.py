952992cbba9440afb506332b4868ced75e432d2c
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-06 19:53:15 +0900

    sleeping

--- sleeping.py
+++ sleeping.py
@@ -1,10 +1,21 @@
 import codecs
 
-htmlpaths = ['index.html', 'how-to-scramble/index.html', 'policy/index.html', 'request/index.html']
+htmlpaths = ['index.html']
+replaceelements = [['menuformat.html', 'MENU_REPLACE_HERE']]
+
+events = ['Clock', 'Mirror3x3', 'Floppy', 'SuperFloppy', 'FloppyGhost', 'Void']
+for event in events:
+    htmlpaths.append(event + '/index.html')
 
 html = ''
 with codecs.open('sleeping.html', 'r', 'utf-8', 'ignore') as f:
     html = f.read()
 for htmlpath in htmlpaths:
-    with codecs.open(htmlpath, 'w', 'utf-8', 'ignore') as f:
-        f.write(html)
+    for replacepath in replaceelements:
+        replaceelement = ''
+        with codecs.open(replacepath[0], 'r', 'utf-8', 'ignore') as f:
+            replaceelement = f.read()
+        html = html.replace(replacepath[1], replaceelement)
+
+        with codecs.open(htmlpath, 'w', 'utf-8', 'ignore') as f:
+            f.write(html)
