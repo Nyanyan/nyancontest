548727d338093bc962f0dba87fc83169c71251fa
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-06 14:54:16 +0900

    menu

--- updatemenu.py
+++ updatemenu.py
@@ -3,15 +3,21 @@ import codecs
 htmlpath = [['home.html', 'index.html']]
 replaceelements = [['menuformat.html', 'MENU_REPLACE_HERE']]
 
-for path, replacepath in zip(htmlpath, replaceelements):
-    print(path, replacepath)
-    html = ''
-    with codecs.open(path[0], 'r', 'utf-8', 'ignore') as f:
-        html = f.read()
-    replaceelement = ''
-    with codecs.open(replacepath[0], 'r', 'utf-8', 'ignore') as f:
-        replaceelement = f.read()
-    html.replace(replacepath[1], replaceelement)
+for path in htmlpath:
+    for replacepath in replaceelements:
+        print(path, replacepath)
 
-    with codecs.open(path[1], 'w', 'utf-8', 'ignore') as f:
-        f.write(html)
+        html = ''
+        with codecs.open(path[0], 'r', 'utf-8', 'ignore') as f:
+            html = f.read()
+        
+        replaceelement = ''
+        with codecs.open(replacepath[0], 'r', 'utf-8', 'ignore') as f:
+            replaceelement = f.read()
+        
+        #print(replacepath[1])
+        html = html.replace(replacepath[1], replaceelement)
+        #print(html)
+
+        with codecs.open(path[1], 'w', 'utf-8', 'ignore') as f:
+            f.write(html)
