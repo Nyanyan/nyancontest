b6e0d434a8cd4ad844ee76c0cabb172c26e6217d
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-22 18:55:39 +0900

    rank

--- makehtml.py
+++ makehtml.py
@@ -38,12 +38,17 @@ for i in range(len(scramblearr)):
         break
 html = html.replace('SCRAMBLE_REPLACE_HERE', scramble)
 
-ranking = ''
+ranking = '<tr><td>順位</td><td>日時</td><td>名前</td><td>1st</td><td>2nd</td><td>3rd</td><td>4th</td><td>5th</td><td>平均</td></tr><tr>'
 rankingarr = []
 with codecs.open(event + "/data.csv", 'r', 'utf-8', 'ignore') as f:
     rankingarr = list(csv.reader(f))
+rank = 1
+tmp = '0'
 for i in range(len(rankingarr)):
-    ranking += '<tr><td>日時</td><td>名前</td><td>1st</td><td>2nd</td><td>3rd</td><td>4th</td><td>5th</td><td>平均</td></tr><tr>'
+    if tmp != rankingarr[i][7]:
+        rank = i + 1
+        tmp = rankingarr[i][7]
+    ranking += '<tr><td>' + str(rank) + '</td>'
     for j in range(len(rankingarr[i])):
         ranking += '<td>' + str(rankingarr[i][j]) + '</td>'
     ranking += '</tr>'
