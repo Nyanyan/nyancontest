7c05fb0905fc04e922768d547f70235232098791
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-03-24 17:03:27 +0900

    everything seems ok, next, test

--- makeallhtml.py
+++ makeallhtml.py
@@ -4,6 +4,6 @@ events = ['Clock', 'Mirror3x3', 'Floppy', 'SuperFloppy', 'FloppyGhost', 'Void', 
 
 for arg in events:
     print(arg)
-    command = ['python', 'makehtml.py', arg]
+    command = ['python3', 'makehtml.py', arg]
     subprocess.run(command)
 
